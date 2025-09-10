132926982d8e3e6c92248b28c4be2aa1e812e1f2
Hamid Sherzad <Hamid.sherzad@iths.se>
2025-09-10 13:14:54 +0200

    Hej

---
+++ Test 2.py
@@ -0,0 +1,5 @@
+gravity = 9.82
+mass = float(input(73))
+force= mass*gravity
+print(f"{force}N")
+print(f"{65}N")
---
+++ Test.py
@@ -0,0 +1,11 @@
+import math
+
+a = 3
+b = 4
+
+c = (math.sqrt(a**2+b**2))
+
+print(f"länged av hypotenusan är {math.sqrt(a**2+b**2)}")
+
+# eller alternativt för att få fram svaret.
+print(c)
