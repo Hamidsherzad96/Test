846790ed7ec5e8e50dd3ddabc00f704435b4bb45
Hamid Sherzad <Hamid.sherzad@iths.se>
2025-09-10 14:10:42 +0200

    hej

--- Test.py
+++ Test.py
@@ -1,3 +1,4 @@
+#Övningar, count with python, a:
 import math
 
 a = 3
@@ -8,4 +9,14 @@ c = (math.sqrt(a**2+b**2))
 print(f"länged av hypotenusan är {math.sqrt(a**2+b**2)}")
 
 # eller alternativt för att få fram svaret.
+print(c)
+
+# b:
+import math
+
+c = 7
+a = 5
+
+b = (math.sqrt(c**2+a**2))
+
 print(c)
